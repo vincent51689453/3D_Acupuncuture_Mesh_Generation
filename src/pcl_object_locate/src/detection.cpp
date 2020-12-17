22277cf06335cc17f8c14a6733815a23b0b2e311
vincent51689453 <vincent51689453@gmail.com>
2020-12-17 17:31:33 +0800

    pass through filter OK!

--- src/pcl_object_locate/src/detection.cpp
+++ src/pcl_object_locate/src/detection.cpp
@@ -5,30 +5,48 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
 #include <iostream>
 
-
+float voxel_leaf_szie = 0.01; //in terms of meter
+int z_pass_min = -1;          //in terms of meter
+int z_pass_max = 1;           //in terms of meter
+  
 ros::Publisher pub;
 
 void cloud_cb (sensor_msgs::PointCloud2 cloud_msg)
 {
+    //Create a new point cloud for storing raw input
     pcl::PointCloud<pcl::PointXYZ> raw_input_cloud;
     pcl::fromROSMsg(cloud_msg, raw_input_cloud);
 
+    //1. VOXEL GRID FILTER [START]
+    //Pointer for raw input and output of voxel filter
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr (new pcl::PointCloud<pcl::PointXYZ> (raw_input_cloud));
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_voxel_filtered (new pcl::PointCloud<pcl::PointXYZ> ());
 
     pcl::VoxelGrid<pcl::PointXYZ> voxel_filter;
     voxel_filter.setInputCloud (cloud_ptr);
-    voxel_filter.setLeafSize (0.01f,0.01f,0.01f);
+    voxel_filter.setLeafSize (voxel_leaf_szie,voxel_leaf_szie,voxel_leaf_szie);
     voxel_filter.filter (*cloud_voxel_filtered);
+    //1. VOXEL GRID FILTER [END]
 
-    //TO-DO Pass through filter
+    //2. PASS THROUGH FILTER [START]
+    //Create a new point cloud for storing filter output
+    pcl::PointCloud<pcl::PointXYZ> zf_cloud;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr passz_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>(zf_cloud));
 
-    sensor_msgs::PointCloud2::Ptr output_msg(new sensor_msgs::PointCloud2);
+    pcl::PassThrough<pcl::PointXYZ> pass_z;
+    pass_z.setInputCloud(cloud_voxel_filtered);
+    pass_z.setFilterFieldName("z");
+    pass_z.setFilterLimits(z_pass_min,z_pass_max);
+    pass_z.filter(zf_cloud);
+    //2. PASS THROUGH FILTER [END]
 
-    pcl::toROSMsg(*cloud_voxel_filtered,*output_msg);
     // Publish the data
+    sensor_msgs::PointCloud2::Ptr output_msg(new sensor_msgs::PointCloud2);
+    pcl::toROSMsg(zf_cloud,*output_msg);
+
     pub.publish (output_msg);
 }
 
