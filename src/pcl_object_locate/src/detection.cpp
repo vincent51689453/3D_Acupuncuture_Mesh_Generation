a2b8e4c91dae9edaa5dfbcdc15a76e3e41fb20aa
vincent <vincent51689453@gmail.com>
2020-12-18 10:59:50 +0800

    plannar segmentation using RANSAC algorithm ok!

--- src/pcl_object_locate/src/detection.cpp
+++ src/pcl_object_locate/src/detection.cpp
@@ -1,23 +1,38 @@
 #include <ros/ros.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+#include <sensor_msgs/PointCloud2.h> 
+
 // PCL specific includes
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h> 
+#include "pcl_ros/transforms.h"
+
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/passthrough.h>
-#include <iostream>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <tf_conversions/tf_eigen.h>
+#include <pcl/segmentation/extract_polygonal_prism_data.h>
 
-float voxel_leaf_szie = 0.01; //in terms of meter
-int z_pass_min = -1;          //in terms of meter
-int z_pass_max = 1;           //in terms of meter
-  
+float voxel_leaf_szie = 0.01;  //in terms of meter
+int z_pass_min = -1;           //in terms of meter
+int z_pass_max = 1;            //in terms of meter
+int plane_max_iter = 50;
+float plane_dist_thresh = 0.01; 
+
 ros::Publisher pub;
 
 void cloud_cb (sensor_msgs::PointCloud2 cloud_msg)
 {
     //Create a new point cloud for storing raw input
     pcl::PointCloud<pcl::PointXYZ> raw_input_cloud;
+    //Convert ros msg into PCL data
     pcl::fromROSMsg(cloud_msg, raw_input_cloud);
 
     //1. VOXEL GRID FILTER [START]
@@ -34,7 +49,6 @@ void cloud_cb (sensor_msgs::PointCloud2 cloud_msg)
     //2. PASS THROUGH FILTER [START]
     //Create a new point cloud for storing filter output
     pcl::PointCloud<pcl::PointXYZ> zf_cloud;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr passz_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>(zf_cloud));
 
     pcl::PassThrough<pcl::PointXYZ> pass_z;
     pass_z.setInputCloud(cloud_voxel_filtered);
@@ -43,9 +57,52 @@ void cloud_cb (sensor_msgs::PointCloud2 cloud_msg)
     pass_z.filter(zf_cloud);
     //2. PASS THROUGH FILTER [END]
 
+    //3. PLANE SEGMENTATION [START]
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cropped_cloud(new pcl::PointCloud<pcl::PointXYZ>(zf_cloud));
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZ> ());
+
+    // Create the segmentation object for the planar model and set all the parameters
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+    seg.setOptimizeCoefficients (true);
+    seg.setModelType (pcl::SACMODEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setMaxIterations (plane_max_iter);
+    seg.setDistanceThreshold (plane_dist_thresh);
+    // Segment the largest planar component from the cropped cloud
+    seg.setInputCloud (cropped_cloud);
+    seg.segment (*inliers, *coefficients);
+    if (inliers->indices.size () == 0)
+    {
+        ROS_WARN_STREAM ("Could not estimate a planar model for the given dataset.") ;
+        //break;
+    }
+
+    // Extract the planar inliers from the input cloud
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+    extract.setInputCloud (cropped_cloud);
+    extract.setIndices(inliers);
+    extract.setNegative (false);
+
+    // Get the points associated with the planar surface
+    extract.filter (*cloud_plane);
+    ROS_INFO_STREAM("PointCloud representing the planar component: " << cloud_plane->points.size () << " data points." );
+
+    // Remove the planar inliers, extract the rest
+    extract.setNegative (true);
+    extract.filter (*cloud_f);
+    //3. PLANE SEGMENTATION [END]
+
+    //4. Euclidean cluster extraction [START]
+    //TO-DO
+    //4. Euclidean cluster extraction [END]
+
     // Publish the data
     sensor_msgs::PointCloud2::Ptr output_msg(new sensor_msgs::PointCloud2);
-    pcl::toROSMsg(zf_cloud,*output_msg);
+    // Convert PCL to ros msg
+    pcl::toROSMsg(*cloud_f,*output_msg);
 
     pub.publish (output_msg);
 }
