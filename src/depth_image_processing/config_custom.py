87c5aa3aa251127b5e7bef165ff4f674ce906e3b
vincent51689453 <vincent51689453@gmail.com>
2021-01-12 16:09:56 +0800

    updated config comments

--- src/depth_image_processing/config_custom.py
+++ src/depth_image_processing/config_custom.py
@@ -16,6 +16,9 @@ alpha_map = 255 / (doi*10)
 
 """
 ------------------------- Image Caputre Configuration -------------------------
+1. img_width  : number of horizontal pixels
+2. img_height : number of vertical pixels
+3. fps        : frame per second
 """
 img_width = 640
 img_height = 480
@@ -24,6 +27,8 @@ fps = 30
 
 """
 ------------------------- Detection Configuration -------------------------
+1. shaddow_hsv_L : shaddows color of depth image after HSV conversion (lower limit)
+2. shaddow_hsv_H : shaddows color of depth image after HSV conversion (upper limit)
 """
 shaddow_hsv_L = [118, -10, -40]
 shaddow_hsv_H = [138, 10, 40]
