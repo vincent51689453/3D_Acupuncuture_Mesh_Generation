677823d7c5b296f440dd910f407c29d7c0fbff0e
vincent51689453 <vincent51689453@gmail.com>
2021-01-12 12:20:22 +0800

    no longer pcl, depth image now

---
+++ src/depth_image_processing/config_custom.py
@@ -0,0 +1,22 @@
+"""
+Colormap Configuration
+
+1. doi : distance of interest in [cm]
+It is indicating the range of distance that will be included in the color map. If d >= doi, they will be treated as the same color in the map. 
+The map will only conisder < doi.
+
+2. alpha_map : parameter for cv2.convertScaleAbs(depth_image, alpha=??)  in [mm]
+This parameter represents the conversion from doi to alpha of cv2.convertScaleAbs
+
+In, cv2. convertScaleAbs, valid_distance*alpha = 255.
+"""
+doi = 43
+alpha_map = 255 / (doi*10)
+
+
+"""
+Image Caputre Configuration
+"""
+img_width = 640
+img_height = 480
+fps = 30
---
+++ src/depth_image_processing/core.py
@@ -0,0 +1,48 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import config_custom as setup
+
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.depth, setup.img_width, setup.img_height, rs.format.z16, setup.fps)
+config.enable_stream(rs.stream.color, setup.img_width, setup.img_height, rs.format.bgr8, setup.fps)
+
+# Start streaming
+pipeline.start(config)
+
+try:
+    while True:
+
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        if not depth_frame or not color_frame:
+            continue
+
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=setup.alpha_map), cv2.COLORMAP_JET)
+
+        # Stack both images horizontally
+        images = np.hstack((color_image, depth_colormap))
+
+        # Show images
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', images)
+        key = cv2.waitKey(1)
+
+        # Press esc or 'q' to close the image window
+        if key & 0xFF == ord('q') or key == 27:
+            cv2.destroyAllWindows()
+            break
+
+finally:
+
+    # Stop streaming
+    pipeline.stop()
